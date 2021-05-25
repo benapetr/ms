2fea43028f5c7ffcc1961c9dbe40e70988fbec8d
Petr Bena <benapetr@gmail.com>
2021-05-25 18:06:59 +0200

    updated git setup

--- setup_git.sh
+++ setup_git.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 git config --global core.editor vim
+git config --global pull.rebase true
 git config --global user.name "Petr Bena"
 git config --global user.email "benapetr@gmail.com"
